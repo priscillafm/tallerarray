72db1d650ac5210ac380924c824905f55b307617
priscillafm <priscillaferreri@gmail.com>
2024-09-03 18:52:03 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  // Más info de forEach => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  // Escribe tu solución aquí
+  // Sugerencia de cómo mostrar el array => showList(strangeArray);
+
+
+  //Filtrar los strings mientras pasen la prubba del typeof
+  const newArray = strangeArray.filter(element => typeof element === "string");
+
+  //Toma le newArray y los orrdena alfabeticamente -toLowerCase convierte strings en minuscula y --localCompaere  compara los strings
+  const newArrayalf = newArray.sort((a, b) => a.toLocaleLowerCase().localCompare(b.toLowerCase()));
+
+
+  //muestra la lista lista ordenada
+  showList(newArrayalf);
+
+
+});
